2c24f462b01fb4b2ab70ddcc6673d7483e02e61d
ffyf <2743967545@qq.com>
2022-06-23 14:09:47 +0800

    hh

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@
  * @Author: zhang
  * @Date: 2022-06-05 15:24:32
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-06-05 15:25:59
+ * @LastEditTime: 2022-06-23 14:05:10
  * @Descripttion: 
  */
 // vue.config.js
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    publicPath: './',
 }
